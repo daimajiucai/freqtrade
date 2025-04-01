5aad86b255aa0b59bd58da8322e8b02910659b93
daimajiucai <93907526+daimajiucai@users.noreply.github.com>
2025-04-01 21:12:20 +0800

    user_data放入远程目录

--- run.py
+++ run.py
@@ -1,24 +1,32 @@
+# -*- coding: utf-8 -*-
 # run.py
 import sys
 from freqtrade.main import main
+import os
 
 def run_robot():
+    os.environ["PYTHONUTF8"] = "1" # 设置环境变量，确保 Python 使用 UTF-8 编码
+    # 设置工作目录（可选）
+    #os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
+    # 配置路径（根据系统选择）  
+    # Windows UNC 路径示例
+    config_path = r"//192.168.123.62/share/user_data/config.json"
+    # Linux/macOS 路径示例ewewe
+    # config_path = "/mnt/知识中心/user_data/config.json"
     # 模拟命令行参数
+    # 验证路径是否存在
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"配置文件不存在: {config_path}")
+    else:
+        print(f"找到配置文件: {config_path}")
     sys.argv = [
         "freqtrade",
         "trade",
-        "--config", "user_data/config.json",
+        "--config", config_path,
         "--strategy", "SampleStrategy"
     ]
     main()
-    # import ccxt
-    # exchange = ccxt.okx({
-    #     'options': {'defaultType': 'future'},
-    #     'proxies': {'http': 'http://127.0.0.1:7890', 'https': 'http://127.0.0.1:7890'}
-    # })
-    # markets = exchange.fetch_tickers()  # 尝试获取全市场数据
-    # print(len(markets))  # 如果返回空或少量数据，说明不支持
-
 
 
 if __name__ == "__main__":
